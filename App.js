9f57f6fdf15390435db4d3af5c2e19abad8c7620
Pierre Peinado <pierre.peinado@webinage.com>
2018-02-27 19:14:48 +0100

    set moment locale to 'fr' globally

--- App.js
+++ App.js
@@ -3,8 +3,14 @@ import SearchStackNavigator from './Navigation/Navigation'
 import { StyleSheet } from 'react-native'
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
+import * as moment from 'moment'
+import 'moment/locale/fr'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    moment.locale('fr')
+  }
   render() {
     return (
       <Provider store={Store}>
